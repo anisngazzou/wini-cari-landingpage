281a8f5aac1e93d4077d34c702b66546f2c16c02
anisngazzou <nole.anis@gmail.com>
2019-10-21 03:13:47 +0100

    Updates

---
+++ precache-manifest.848857636a13423781935f9ed77282e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45d0a7b098aefacb675744a5c4bedd1",
+    "url": "/wini-cari-landingpage/index.html"
+  },
+  {
+    "revision": "4f145991d9a11d2a8772",
+    "url": "/wini-cari-landingpage/static/css/main.363ae5f0.chunk.css"
+  },
+  {
+    "revision": "fbf2fd115261d4363987",
+    "url": "/wini-cari-landingpage/static/js/2.d03c0aa7.chunk.js"
+  },
+  {
+    "revision": "4f145991d9a11d2a8772",
+    "url": "/wini-cari-landingpage/static/js/main.27657f6c.chunk.js"
+  },
+  {
+    "revision": "2197404a62862da2e488",
+    "url": "/wini-cari-landingpage/static/js/runtime-main.3968ab8d.js"
+  }
+]);
